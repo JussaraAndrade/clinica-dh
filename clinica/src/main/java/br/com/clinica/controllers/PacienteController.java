c03126a8d247181d3129579febd13f8fc6a7df98
Jussara <jussara.jandrade1993@outlook.com>
2020-10-17 21:26:26 -0300

    resolvendo conflitos

--- clinica/src/main/java/br/com/clinica/controllers/PacienteController.java
+++ clinica/src/main/java/br/com/clinica/controllers/PacienteController.java
@@ -2,10 +2,7 @@ package br.com.clinica.controllers;
 
 import java.util.Optional;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,53 +20,60 @@ import br.com.clinica.services.RequisitaConsultaService;
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
-	
+
 	@Autowired
 	PacienteRepository pacienteRepository;
-	
+
 	RequisitaConsultaService requisitaConsulta;
-	
+
 	@GetMapping
 	public Iterable<Paciente> getPacientes() {
 		return pacienteRepository.findAll();
 	}
-	
+
 	@GetMapping("/{id}")
-	public Optional<Paciente> getById(@PathVariable Integer id){
+	public Optional<Paciente> getById(@PathVariable Integer id) {
 		return pacienteRepository.findById(id);
 	}
-	
+
 	@PostMapping("/requisita/{nome}")
 	public void requesitaConsulta(@RequestParam String medico, @RequestBody Paciente paciente) {
 		System.out.println(medico);
 		requisitaConsulta.addConsulta(paciente, medico);
 	}
-	
+
 	@PostMapping
 	public void addPaciente(@RequestBody Paciente paciente) {
 		pacienteRepository.save(paciente);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public void deletaPaciente(@PathVariable Integer id) {
 		pacienteRepository.deleteById(id);
 	}
-	
+
 	@PutMapping("/{idPaciente}")
-	public Paciente updatePaciente(@PathVariable int idPaciente, @RequestBody Paciente dadosPaciente) throws Exception{
-		Paciente pacienteDB = pacienteRepository.findById(idPaciente)
-				.orElseThrow(() -> new IllegalAccessException());
-		if(dadosPaciente.getNome() != null) pacienteDB.setNome(dadosPaciente.getNome());
-		if(dadosPaciente.getCpf() != null)pacienteDB.setCpf(dadosPaciente.getCpf());
-		if(dadosPaciente.getPeso() != null)pacienteDB.setPeso(dadosPaciente.getPeso());
-		if(dadosPaciente.getEmail()!= null)pacienteDB.setEmail(dadosPaciente.getEmail());
-		if(dadosPaciente.getData_nascimento()!= null)pacienteDB.setData_nascimento(dadosPaciente.getData_nascimento());
-		if(dadosPaciente.getTelefone()!= null)pacienteDB.setTelefone(dadosPaciente.getTelefone());
-		if(dadosPaciente.getAltura()!= null)pacienteDB.setAltura(dadosPaciente.getAltura());
-		if(dadosPaciente.getData_primeira_consulta()!= null)pacienteDB.setData_primeira_consulta(dadosPaciente.getData_primeira_consulta());
-		if(dadosPaciente.getEndereco()!= null)pacienteDB.setEndereco(dadosPaciente.getEndereco());
-		
+	public Paciente updatePaciente(@PathVariable int idPaciente, @RequestBody Paciente dadosPaciente) throws Exception {
+		Paciente pacienteDB = pacienteRepository.findById(idPaciente).orElseThrow(() -> new IllegalAccessException());
+		if (dadosPaciente.getNome() != null)
+			pacienteDB.setNome(dadosPaciente.getNome());
+		if (dadosPaciente.getCpf() != null)
+			pacienteDB.setCpf(dadosPaciente.getCpf());
+		if (dadosPaciente.getPeso() != null)
+			pacienteDB.setPeso(dadosPaciente.getPeso());
+		if (dadosPaciente.getEmail() != null)
+			pacienteDB.setEmail(dadosPaciente.getEmail());
+		if (dadosPaciente.getData_nascimento() != null)
+			pacienteDB.setData_nascimento(dadosPaciente.getData_nascimento());
+		if (dadosPaciente.getTelefone() != null)
+			pacienteDB.setTelefone(dadosPaciente.getTelefone());
+		if (dadosPaciente.getAltura() != null)
+			pacienteDB.setAltura(dadosPaciente.getAltura());
+		if (dadosPaciente.getData_primeira_consulta() != null)
+			pacienteDB.setData_primeira_consulta(dadosPaciente.getData_primeira_consulta());
+		if (dadosPaciente.getEndereco() != null)
+			pacienteDB.setEndereco(dadosPaciente.getEndereco());
+
 		return pacienteRepository.save(pacienteDB);
 	}
 }
-	
